111e2a46885406507c3688a394724ee24aadfc61
nay <nayeli.guzman@utec.edu.pe>
2025-11-14 21:37:46 -0500

    add: base

---
+++ outputs/input_1.s
@@ -0,0 +1,43 @@
+.data
+print_fmt: .string "%ld \n"
+x: .quad 0
+.text
+.globl suma
+suma:
+ pushq %rbp
+ movq %rsp, %rbp
+ movq %rdi,-8(%rbp)
+ movq %rsi,-16(%rbp)
+ subq $24, %rsp
+ movq -8(%rbp), %rax
+ pushq %rax
+ movq -16(%rbp), %rax
+ movq %rax, %rcx
+ popq %rax
+ addq %rcx, %rax
+ movq %rax, -24(%rbp)
+ movq -24(%rbp), %rax
+ jmp .end_suma
+.end_suma:
+leave
+ret
+.globl main
+main:
+ pushq %rbp
+ movq %rsp, %rbp
+ subq $0, %rsp
+ movq $2, %rax
+ mov %rax, %rdi
+ movq $3, %rax
+ mov %rax, %rsi
+call suma
+ movq %rax, %rsi
+ leaq print_fmt(%rip), %rdi
+ movl $0, %eax
+ call printf@PLT
+ movq $0, %rax
+ jmp .end_main
+.end_main:
+leave
+ret
+.section .note.GNU-stack,"",@progbits
